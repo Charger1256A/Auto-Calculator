5dd6b6997a5077ac9fb964317d46dbb667893813
Zachary Brandt <zachary@zachary-macbook-air.local>
2020-02-14 19:48:09 -0800

    tba API config

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const json = require('../matchdata.json');
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
-  
+  res.render('index', { title: 'Express' });
+  console.log("hello")
 });
-
-
+router.get('/', function(req, res, next) {
+  res.send('value', { array: json })
+})
 module.exports = router;
---
+++ routes/tbaAPI.js
@@ -0,0 +1,42 @@
+const request = require('request');
+const fs = require('fs');
+function pathData(path) {
+    return new Promise((resolve, reject) => {
+        var list = {
+            url: `https://www.thebluealliance.com/api/v3${path}`,
+            method: 'GET',
+            headers: {
+                'X-TBA-Auth-Key': 'oSSXMWPE2jOJrLTYpgMvgP5BTvbtOJRwR6LSv1ytb0g5FS6RlaWBx70Pw0B8cwvA'
+            }
+        };
+        request(list, function (err, res, body) {
+            resolve(JSON.parse(body));
+        });
+    });
+};
+function matchData(eventId) {
+    return new Promise((resolve, reject) => {
+        pathData(`/event/${eventId}/matches`).then(snapshot => {
+            var array = [];
+            for (var i in snapshot) {
+                if ("qm" == snapshot[i]["comp_level"]) {
+                    var object = {
+                        match: snapshot[i]["match_number"],
+                        blue: snapshot[i]["alliances"]["blue"]["team_keys"].map(x => x.replace('frc', '')),
+                        red: snapshot[i]["alliances"]["red"]["team_keys"].map(x => x.replace('frc', ''))
+                    }
+                    array.push(object);
+                }
+            }
+            resolve(array);
+        });
+    });
+}
+matchData('2019utwv').then(snapshot => {
+    fs.writeFile('matchdata.json', JSON.stringify(snapshot), function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+});
+exports.matchData = matchData
+exports.pathData = pathData
